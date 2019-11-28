e1fdfc579df0ae25414136a6b3e15fee28858488
Masmae <mmasmaaa1999@outlook.com>
2019-11-28 03:45:02 -0800

    plants class - proximity

---
+++ src/Proximity.java
@@ -0,0 +1,22 @@
+public class Proximity {
+    private int distance;
+    private int load;
+    private boolean isTall;
+    private int damage;
+    private int sunProduced;
+    private boolean mine;
+    private boolean isMagnet;
+
+    public static void cattail() {
+        //kills the nearest zombie
+    }
+
+    public static void cherryBomb() {
+        //bursts 8 neighbor cells
+    }
+
+    public static void jalapeno() {
+        //bursts the row
+    }
+
+}
