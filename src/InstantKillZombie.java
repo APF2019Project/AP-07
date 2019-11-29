7c56d04e6c84b9283b965310c0cd54c7a40e3b48
doctorhoseinpour <45894816+doctorhoseinpour@users.noreply.github.com>
2019-11-29 15:49:07 +0330

    plants+zombies+player+cell+methods

--- src/InstantKillZombie.java
+++ src/InstantKillZombie.java
@@ -2,4 +2,12 @@ public class InstantKillZombie {
     private int distance;
     private boolean IsZomboni;
     private boolean isBungeeZombie;
+    public void StealPlant()
+    {
+        //takes a fucking plant
+    }
+    public void TurnIntoCatapult()
+    {
+        //zomboni turns into a fucking catapult
+    }
 }
--- src/NormalZombie.java
+++ src/NormalZombie.java
@@ -5,4 +5,8 @@ public class NormalZombie extends Zombie {
     private int distance;
     private boolean isPogoZombie;
     private boolean isBaloonZombie;
+    public void JumpOverPlants()
+    {
+        //az giaha rad mishe che vase baloon zombie che vase pogo zombie
+    }
 }
