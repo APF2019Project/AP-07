55c3fbccc91e43ccc22c3ddb4e96b2540043dcde
Masmae <M_asmae@outlook.com>
2019-12-18 05:06:44 -0800

    ...

--- src/Model/Card/Card.java
+++ src/Model/Card/Card.java
@@ -6,14 +6,26 @@ import Model.Shop.Collection;
 import java.util.ArrayList;
 
 public class Card {
-    String name;
+    protected String name;
     int id;
-    private static int uniqueId=0;
+    private static int uniqueId = 0;
     int AP;//attack point
     int HP;//hosseinpur
     Cell location;
     Collection collection;
+    int coin;
 
     private static ArrayList<Card> cards;
+
+    public int getCoin() {
+        return this.coin;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
