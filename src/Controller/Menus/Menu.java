aebaf708433be059653c6e6a01a47aebacfb0ec4
Masmae <M_asmae@outlook.com>
2019-12-19 05:38:46 -0800

    Menu

--- src/Controller/Menus/Menu.java
+++ src/Controller/Menus/Menu.java
@@ -11,34 +11,17 @@ public class Menu {
     public static ProfileMenu profileMenu = new ProfileMenu();
     public static ShopMenu shopMenu = new ShopMenu();
     public static MenuHandler menuHandler = new MenuHandler();
-    //Account account = new Account()
-    Menu parentMenu =null;
-    Menu currentMenu=null;
-    ArrayList<String> orders=new ArrayList<>();
+    Menu parentMenu = null;
+    Menu currentMenu = null;
+    ArrayList<String> orders = new ArrayList<>();
 
-    public Menu(){
-        currentMenu=this;
-    }
-
-    //namayeshe tamame dastoorate menu
-    public void help(){
-        for(int i=0;i<orders.size();i++){
-            System.out.println(orders.get(i));
+    public void help() {
+        for (String x : menuHandler.getCurrentMenu().orders) {
+            System.out.println(x);
         }
     }
 
-    public void init(){
-        //enter
-        // kar haye avaliye mesl set kardan e account o ina
-    }
-
-    public void exit(){
-        currentMenu=parentMenu;
-        //bazgasht be safheye qabl
-    }
-
-    public void enter(Menu menu){
-        //inito seda kon
-        currentMenu=this;
+    public static void init() {
+        menuHandler.setCurrentMenu(loginMenu);
     }
 }
