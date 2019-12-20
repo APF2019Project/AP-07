64559cd2e7bbdd30dd9bdb35bf852062241b6bc5
Masmae <M_asmae@outlook.com>
2019-12-20 00:30:10 -0800

    Main

--- src/Model/Card/Zombies/Snorkel.java
+++ src/Model/Card/Zombies/Snorkel.java
@@ -1,5 +1,13 @@
 package Model.Card.Zombies;
 
+import Model.Card.Plants.PeaOrProjectile;
+import Model.Map.Cell;
+
 public class Snorkel extends Zombie {
+
     private int armour;
+
+    public Snorkel(String name, int AP, int HP, Cell cell, int health, int speed, PeaOrProjectile peaOrProjectile) {
+        super(name, AP, HP, cell, health, speed, peaOrProjectile);
+    }
 }
