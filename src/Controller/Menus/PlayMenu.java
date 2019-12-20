2da36f0aad72514bf5b0865ab2140ab52c7e49bf
doctorhoseinpour <45894816+doctorhoseinpour@users.noreply.github.com>
2019-12-20 17:01:15 +0330

    Json

--- src/Controller/Menus/PlayMenu.java
+++ src/Controller/Menus/PlayMenu.java
@@ -6,7 +6,6 @@ public class PlayMenu extends Menu {
     public PlayMenu(){
         this.orders = new String[]{"Day", "Water", "Rail", "Zombie", "PvP"};
     }
-
     public void startDayGame() {
 
     }
