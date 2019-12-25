a6076060702aeedcf095aa899656dcd228b2bea0
sarakh99 <sampadsara@gmail.com>
2019-12-25 16:17:32 +0330

    u

--- src/Model/Card/Zombies/ZombiesActions/InstantKill.java
+++ src/Model/Card/Zombies/ZombiesActions/InstantKill.java
@@ -1,14 +1,10 @@
 package Model.Card.Zombies.ZombiesActions;
 
-import Model.Card.Plants.Plant;
 import Model.Card.Zombies.Zombie;
-import Model.Map.UnknownCell;
 
 public class InstantKill {
     public void Kill(Zombie zombie)
     {
-        /// evnte
-        UnknownCell cell =  zombie.getCell();
-        cell.plant(null);
+
     }
 }
