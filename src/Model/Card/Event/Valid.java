20b59f06fd939e5a7a324687e1078dd02ae13bea
doctorhoseinpour <45894816+doctorhoseinpour@users.noreply.github.com>
2020-01-03 14:55:38 +0330

    event ham doros shd ta andaze e

--- src/Model/Card/Event/Valid.java
+++ src/Model/Card/Event/Valid.java
@@ -1,11 +1,12 @@
 package Model.Card.Event;
 
+import Controller.GameMode.Battle;
 import Model.Card.Plants.Plant;
 import Model.Map.Map;
 
 public class Valid extends Event {
     @Override
-    public boolean check(Plant plant, Map map, int d) {
+    public boolean check(Plant plant, Battle battle, int d) {
         return true;
     }
 }
