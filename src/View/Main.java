1263f877de95609229a6a5729a6d841f0290680b
Masmae <m.asmae@studen.sharif.ir>
2020-01-30 18:24:52 +0330

    fxml

--- src/View/Main.java
+++ src/View/Main.java
@@ -10,8 +10,13 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+import javax.imageio.stream.FileImageInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
 
 import static javafx.application.Platform.exit;
 
@@ -22,41 +27,42 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-//        Group root = new Group();
-//        Scene scene = new Scene(root, sceneWitdh, sceneWitdh);
-//        Image image = new Image(new FileInputStream("C:\\Users\\asus\\IdeaProjects\\AP-07\\AP-07-1\\src\\Images\\LoginMenu.png"));
-//        ImageView imageView1 = new ImageView(image);
-//        imageView1.setX(0);
-//        imageView1.setY(0);
-//        imageView1.setFitHeight(sceneHeight);
-//        imageView1.setFitWidth(sceneWitdh);
-//        root.getChildren().add(imageView1);
-//
-//        Button quitButton = new Button();
-//        root.getChildren().add(quitButton);
-//        quitButton.relocate(720, 680);
-//        quitButton.setMinHeight(50);
-//        quitButton.setMinWidth(50);
-//        quitButton.setOpacity(0);
-//        quitButton.setOnMouseClicked(event -> {
-//            exit();
-//        });
-//
-//        Button helpButton = new Button();
-//        root.getChildren().add(helpButton);
-//        helpButton.relocate(650, 680);
-//        helpButton.setMinHeight(50);
-//        helpButton.setMinWidth(50);
-//        helpButton.setOpacity(0);
-//        helpButton.setOnMouseClicked(event -> {
-//
-//        });
-//
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
+        Group root = new Group();
+        Scene scene = new Scene(root, sceneWitdh, sceneWitdh);
+        Path path = Paths.get("Images");
+        Image image = new Image("Images\\LoginMenu.png");
+        ImageView imageView1 = new ImageView(image);
+        imageView1.setX(0);
+        imageView1.setY(0);
+        imageView1.setFitHeight(sceneHeight);
+        imageView1.setFitWidth(sceneWitdh);
+        root.getChildren().add(imageView1);
 
-        MenuHandler menuHandler=new MenuHandler();
-        menuHandler.run();
+        Button quitButton = new Button();
+        root.getChildren().add(quitButton);
+        quitButton.relocate(720, 680);
+        quitButton.setMinHeight(50);
+        quitButton.setMinWidth(50);
+        quitButton.setOpacity(0);
+        quitButton.setOnMouseClicked(event -> {
+            exit();
+        });
+
+        Button helpButton = new Button();
+        root.getChildren().add(helpButton);
+        helpButton.relocate(650, 680);
+        helpButton.setMinHeight(50);
+        helpButton.setMinWidth(50);
+        helpButton.setOpacity(0);
+        helpButton.setOnMouseClicked(event -> {
+
+        });
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+//        MenuHandler menuHandler=new MenuHandler();
+//        menuHandler.run();
 
     }
 
