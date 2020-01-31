6331c2ddfcc4087e7bb9849cc68c22d56084b052
sarakh99 <sampadsara@gmail.com>
2020-01-31 13:57:32 +0330

    exit button in fxml

--- src/Controller/Menus/MainMenu.java
+++ src/Controller/Menus/MainMenu.java
@@ -5,29 +5,15 @@ import com.gilecode.yagson.YaGson;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-
-import java.awt.*;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
-
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.VBox;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class MainMenu extends Menu implements Initializable {
-    @FXML
-    public Button start;
-    public Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+public class MainMenu extends Menu  {
 
 
     public MainMenu() throws IOException {
@@ -43,12 +29,6 @@ public class MainMenu extends Menu implements Initializable {
         writer.close();
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        start.setOnAction(actionEvent -> {
-            System.out.println("Bashe Baba");
-        });
 
-    }
 }
 
