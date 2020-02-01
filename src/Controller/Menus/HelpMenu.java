a11d6eede58889cb4b686a9c616a39410c035e60
sarakh99 <sampadsara@gmail.com>
2020-02-01 17:05:47 +0330

    jhajsJuk

---
+++ src/Controller/Menus/HelpMenu.java
@@ -0,0 +1,20 @@
+package Controller.Menus;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javax.xml.soap.Text;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelpMenu extends Menu implements Initializable {
+    public Label label ;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        label.getStyleClass().add("label");
+    }
+
+//    public void hrsahaohif() {
+//        Menu.root =
+//    }
+}
--- src/View/Main.java
+++ src/View/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
-        Parent root = loader.load(getClass().getResource("../Controller/Menus/MainMenu.fxml"));
+        Parent root = loader.load(getClass().getResource("../Controller/Menus/helpMenu.fxml"));
         primaryStage.setTitle("Zombieeeeeeeee");
 
 
